7694c1ada6cb2e8e689431f7adb14e55c13f03a9
tomekpilot@gmail.com <nov99344>
2018-08-31 14:50:00 -0400

    Added initial artifacts for Flight.

--- zz-backups-and-misc/sql/insert-crewmembers.sql
+++ zz-backups-and-misc/sql/insert-crewmembers.sql
@@ -19,11 +19,13 @@ insert into crewmember
     )
 values
     (
-        'Cecily', 'Lyons'
+        'Cecily', 'Lyons','123 Main St. South', 'Suite A','St. Petersburg','FL', '33998', '123-456-6789', false, 'c.l.@m.com','123000-B', 'Notes placeholder', 'Class: WTF?', true, 'cl-password', 'cl-username'
     ),
     (
-        'Rob', 'Gilchrist'
+        'Rob', 'Gilchrist','235 Boulevard St. South', 'Suite A','London','FL', 'AB12', '999-456-6789', false, 'r.g.@mlb.com','qewrwe-B', 'Notes placeholder', 'Class: WTF?', true, 'rg-password', 'rg-username'
+
     ),
     (
-        'Misa', 'Yamaguchi'
+        'Misa', 'Miguchi','123 Main St. South', 'Suite A','Nakajima','FL', 'AB-339', '666-456-6789', false, 'M.Y.@nippon.com','8976-B', 'Notes placeholder', 'Class: WTF?', true, 'my-password', 'my-username'
+
     );
