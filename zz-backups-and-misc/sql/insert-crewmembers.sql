134552ee36c8ba89bbc727c00664dbf9c9c3ac60
tomekpilot@gmail.com <nov99344>
2018-08-31 09:45:30 -0400

    Added initial artifacts for Flight.

---
+++ zz-backups-and-misc/sql/insert-crewmembers.sql
@@ -0,0 +1,29 @@
+insert into crewmember
+    (
+        first_name,
+        last_name,
+        address1,
+        address2,
+        city,
+        state,
+        zip,
+        phone,
+        display_email,
+        email,
+        certificate_no,
+        notes,
+        class,
+        enabled,
+        password,
+        username
+    )
+values
+    (
+        'Cecily', 'Lyons'
+    ),
+    (
+        'Rob', 'Gilchrist'
+    ),
+    (
+        'Misa', 'Yamaguchi'
+    );
---
+++ zz-backups-and-misc/sql/insert-flights.sql
@@ -0,0 +1,34 @@
+INSERT INTO flight
+    (
+        actual_instrument,
+        aircraft_id,
+        aircraft_category_and_class,
+        aircraft_tail_number,
+        aircraft_type,
+        as_flight_instructor,
+        cross_country,
+        date,
+        day,
+        departure,
+        destination,
+        dual_received,
+        extended_flight_details_id,
+        ground_trainer,
+        instructor_id,
+        night,
+        no_day_landings,
+        no_inst_aproaches,
+        no_night_landings,
+        pilot_in_command,
+        remarks,
+        route,
+        safety_pilot_id,
+        second_in_command,
+        simulated_instrument,
+        story_id,
+        total_duration_of_flight
+    )
+values
+    (),
+    (),
+    ();
