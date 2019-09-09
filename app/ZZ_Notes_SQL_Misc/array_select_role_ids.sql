56387acf6c3a6cfe52eab3c582da73c28b4d9a9c
tomekpilot@gmail.com <nov99344>
2019-09-09 11:45:49 -0400

    getting started on the stored procedure

--- app/ZZ_Notes_SQL_Misc/array_select_role_ids.sql
+++ app/ZZ_Notes_SQL_Misc/array_select_role_ids.sql
@@ -3,3 +3,46 @@ select first_name, ARRAY(
 		from crewmember_crewmembertype_xref
 			where crewmember_crewmembertype_xref.crewmember_id = crewmember.id) as "role_ids"
 			from crewmember
+
+
+-- stored proc
+
+
+
+CREATE OR REPLACE PROCEDURE select_crewmembers_with_roles()
+LANGUAGE plpgsql
+AS $$
+    begin
+
+        select
+            first_name,
+            last_name,
+            ARRAY(
+                select crewmembertype_id
+                from crewmember_crewmembertype_xref
+                where crewmember_crewmembertype_xref.crewmember_id = crewmember.id
+                ) as "roles"
+        from
+            crewmember
+
+    end;
+$$;
+
+
+
+-- list of crew members qualified to play certain role
+-- take a ROLE ID as param
+
+CREATE OR REPLACE PROCEDURE select_crewmembers_by_role( INT)
+LANGUAGE plpgsql
+AS $$
+    begin
+
+        select
+            first_name,
+            last_name,
+        from
+            crewmember
+
+    end;
+$$;
